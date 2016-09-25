edb4b58e0566a6436af16876bc0a938fcb4fc9b9
Kovacs Arthur Kraid <kovacs.arthur.kraid@gmail.com>
2016-09-25 17:30:07 +0300

    First push

---
+++ desktop/client/app/app.js
@@ -0,0 +1,31 @@
+import angular from 'angular';
+import 'angular-material/angular-material.css';
+import 'font-awesome/css/font-awesome.css';
+import angularAnimate from 'angular-animate';
+import angularAria from 'angular-aria';
+import angularMaterial from 'angular-material';
+import uiRouter from 'angular-ui-router';
+import Common from './common/common';
+import Components from './components/components';
+import AppComponent from './app.component';
+import 'normalize.css';
+
+//Photos
+import './images/doggo.jpg';
+
+angular.module('app', [
+    angularMaterial,
+    angularAria,
+    angularAnimate,
+    uiRouter,
+    Common,
+    Components
+  ])
+  .config(($locationProvider) => {
+    "ngInject";
+    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
+    // #how-to-configure-your-server-to-work-with-html5mode
+    $locationProvider.html5Mode(true).hashPrefix('!');
+  })
+
+  .component('app', AppComponent);

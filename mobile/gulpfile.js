810c8e0acabd7e5ed8a52a714384eba275378370
Kovacs Arthur Kraid <kovacs.arthur.kraid@gmail.com>
2016-09-25 18:04:55 +0300

    Making Ionic gulp watch for the sass file.

--- mobile/gulpfile.js
+++ mobile/gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
-gulp.task('watch', function() {
+gulp.task('serve:before', function() {
   gulp.watch(paths.sass, ['sass']);
 });
 
